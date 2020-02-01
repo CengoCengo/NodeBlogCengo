18758dd51974d06128679d8b33b3864209b03c37
Cengizhan Eren <cengizhaneren@gmail.com>
2020-02-02 01:35:08 +0300

    Angela ile başlangıç

---
+++ server.js
@@ -0,0 +1,25 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:true})); 
+
+app.get("/", function(req,res){
+  res.sendFile(__dirname + "/index.html");
+})
+
+app.get("/contact", function(req,res){
+  res.send("Burası da iletişim bölümü");
+})
+
+app.get("/naber", function(req,res){
+  res.send("olabilir")
+})
+
+app.listen(3001, function(){
+
+console.log("Server started on port 3000")
+
+});

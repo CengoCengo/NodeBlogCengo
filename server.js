936daaae968fe4857fe0a5326a58aaf74add1c68
Cengizhan Eren <cengizhaneren@gmail.com>
2020-02-02 05:04:38 +0300

    Ejs bölümlemeyee giriş yapıldı

--- server.js
+++ server.js
@@ -4,18 +4,20 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended:true})); 
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req,res){
-  res.sendFile(__dirname + "/index.html");
+  res.render("head");
 })
+
+app.set("view engine", "ejs");
 
 app.get("/contact", function(req,res){
   res.send("Burası da iletişim bölümü");
 })
 
 app.get("/naber", function(req,res){
-  res.send("olabilir")
+  res.render("slider")
 })
 
 app.listen(3001, function(){
